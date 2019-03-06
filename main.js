eb1b76b4fa2ef978e343b70a7caf42d8fbc28d13
Dev Davis <sean.dev.davis@gmail.com>
2019-03-05 19:36:22 -0600

    modified favicon index and js files

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log('Hi Mom');
+const dinosaurs = [
+    {
+        dinoType: 't-rex',
+    },
+    {
+        dinoType: 'stegosaurus',
+    },
+    {
+        dinoType: 'brontosaurus',
+    }
+];
+
+
+
+//loop over the dinosaurs
+//build up a DOMstring
+//print the DOMstring to the DOM
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+}
+
+const buildDinosaurs = () => {
+    let domString = '';
+    for(let i = 0; i < dinosaurs.length; i++){
+        domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+        console.log(dinosaurs[i].dinoType);
+    }
+    printToDom('dino-barn', domString);
+}
+
+const init = () => {
+    buildDinosaurs();
+};
+
+init();
