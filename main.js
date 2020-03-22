3c63ea98ff220cd2f86b7e534717822dcd53b541
Sean Davis <sean.dev.davis@gmail.com>
2020-03-22 13:01:28 -0500

    practicing with js methods on arrays

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const dinosaurs = [
     {
-        dinoType: 't-rex',
-        name:'<span class="saur">Wrex</span>',
+        dinoType: 'T-rex',
+        name:'Wrex',
     },
     {
-        dinoType: 'stegosaurus',
-        name:'<span class="saur">Steve</span>',
+        dinoType: 'Stegosaurus',
+        name:'Steve',
     },
     {
-        dinoType: 'brontosaurus',
-        name: '<span class="saur">Ted</span>',
+        dinoType: 'Brontosaurus',
+        name: 'Ted',
+    },
+    {
+        dinoType: 'Veloceraptor',
+        name: 'Bruno'
     }
 ];
 
@@ -31,10 +35,10 @@ const buildDinosaurs = () => {
 
     for(let i = 0; i < dinosaurs.length; i++){
         domString += `<div class="dinosaur">`;
-        domString +=    `<h3>${dinosaurs[i].dinoType}</h3>`;
+        domString +=    `<h5>${dinosaurs[i].dinoType}</h5>`;
         domString +=    `<p>${dinosaurs[i].name}</p>`;
         domString += `</div>`;
-        console.log(dinosaurs[i].dinoType);
+        // console.log(dinosaurs[i].dinoType);
     }
     printToDom('dino-barn', domString);
 }
@@ -42,49 +46,51 @@ const buildDinosaurs = () => {
 
 const assignments = [
     {
-        title: 'product cards',
+        title: 'Product Cards',
         dueDate: '03/05/2019',
         topic: 'HTML/CSS',
-        notes: 'use flexbox',
+        notes: 'Use Flexbox',
         assigmentURL: 'www.google.com'
     },
     {
-        title: 'monkeys',
+        title: 'House of Wizards',
         dueDate: '04/01/2019',
         topic: 'JavaScript',
-        notes: 'use wizard hat',
+        notes: 'Use wizard hat',
         assignmentURL: 'www.wizards.com'
     },
     {
-        title: 'kittens',
+        title: 'Penn and Teller',
         dueDate: '05/12/2019',
         topic: 'Python',
-        notes: 'use magic wand',
+        notes: 'Use magic wand',
         assignmentURL: 'www.pixies.com'
     },
     {
-        title: 'antelopes',
+        title: 'Runny Noses',
         dueDate: '06/02/2019',
         topic: 'JavaScript',
-        notes: 'use endless handkerchief',
+        notes: 'Use endless handkerchief',
         assignmentURL: 'www.mermaids.com'
     },
     {
-        title: 'zebras',
+        title: 'Super Powers',
         dueDate: '07/03/2019',
         topic: 'PHP',
-        notes: 'use invisibility cloak',
+        notes: 'Use invisibility cloak',
         assignmentURL: 'www.potterheads.com'
     }
 ];
+
+console.log(assignments.length);
 
 const ssignmentCards = () => {
     let domString = '';
     for(let i = 0; i < assignments.length; i++) {
         domString += `<div class="assignment">`;
         domString += `<h5 class="assignment-title">${assignments[i].title}</h5>`;
-        domString += `<div class="due-date"><h3>Due Date: ${assignments[i].dueDate}</h3></div>`;
-        domString += `<p>Get the assignment <a href=${assignments[i].assignmentURL}>HERE</a></p>`;
+        domString += `<div class="due-date"><h6>Due Date: ${assignments[i].dueDate}</h6></div>`;
+        // domString += `<p>Get the assignment <a href=${assignments[i].assignmentURL}>HERE</a></p>`;
         domString += `<p>Topic: ${assignments[i].topic}</p>`;
         domString += `<p>Notes: ${assignments[i].notes}</p>`;
         domString += `</div>`;
